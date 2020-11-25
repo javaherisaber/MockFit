7d2c524361150d1aa1a8523233ce9608e43d9ab0
Mahdi Javaheri <m.javaherisaber@gmail.com>
2020-11-25 13:04:35 +0330

    Remove redundant lines from build.gradle and api.kt

--- app/src/main/java/ir/logicbase/mockfit/app/Api.kt
+++ app/src/main/java/ir/logicbase/mockfit/app/Api.kt
@@ -5,7 +5,6 @@ package ir.logicbase.mockfit.app
 import ir.logicbase.mockfit.Mock
 import retrofit2.Call
 import retrofit2.http.GET
-import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface Api {
